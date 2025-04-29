76404fc588754daaa43e9a403bd923911830d682
abderrahman <karamabderrahmane30@gmail.com>
2025-04-29 21:13:34 +0100

    update:

--- rapport_tps/tp2/ex13.cpp
+++ rapport_tps/tp2/ex13.cpp
@@ -2,34 +2,26 @@
 #include <string>
 
 class Etudiant {
-private:
-    std::string nom;
-    int age;
-
-public:
-    Etudiant(std::string nom, int age) : nom(nom), age(age) {} // ?? constructeurs 
-    void afficher() const {
-        std::cout << "Nom : " << nom << ", age : " << age << std::endl;
-    }
-};
+	private:
+		std::string nom;
+		int age;
+	
+	public:
+		Etudiant() : nom("Inconnu"), age(0) {} // constructeur
+		Etudiant(std::string nom, int age) : nom(nom), age(age) {}
+		void afficher() const {
+			std::cout << "le nom : " << nom << " / l'age : " << age << std::endl;
+		}
+};	
 
 int main() {
     int nombreEtudiants = 3;
-
-    // std::cout << "Entrez le nombre d'étudiants : ";
-    // std::cin >> nombreEtudiants;
-
-    Etudiant *etudiants = new Etudiant[nombreEtudiants]
-	{
-        Etudiant("Alice", 20),
-        Etudiant("Bob", 22),
-        Etudiant("Charlie", 19)
-    };
-
+	Etudiant* etudiants = new Etudiant[nombreEtudiants];
+	etudiants[0] = Etudiant("Abderrahman", 21);
+	etudiants[1] = Etudiant("amal", 22);
+	etudiants[2] = Etudiant("amine", 19);
     for (int i = 0; i < nombreEtudiants; i++)
-	{
         etudiants[i].afficher();
-    }
     delete[] etudiants;
     return 0;
 }
